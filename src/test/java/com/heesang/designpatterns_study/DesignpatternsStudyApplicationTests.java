e224235bcfab51e8f26ef492a9593273e294f2a2
heesang <dlagmltkd12@naver.com>
2022-04-20 13:35:56 +0900

    initial commit

---
+++ src/test/java/com/heesang/designpatterns_study/DesignpatternsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heesang.designpatterns_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
