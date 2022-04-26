dbe2f7e8c8eefd9df7149331da56fd3388c1d27e
heesang <dlagmltkd12@naver.com>
2022-04-26 13:38:12 +0900

    feat : 팩토리 메소드 패턴 SpringBean 사용 예시

---
+++ src/main/java/com/heesang/designpatterns_study/_01_creational_patterns/_02_factory_method/_03_java/SimpleFactory.java
@@ -0,0 +1,17 @@
+package com.heesang.designpatterns_study._01_creational_patterns._02_factory_method._03_java;
+
+import com.heesang.designpatterns_study._01_creational_patterns._02_factory_method._02_after.BlackShip;
+import com.heesang.designpatterns_study._01_creational_patterns._02_factory_method._02_after.WhiteShip;
+
+public class SimpleFactory {
+
+    public Object createProduct(String name) {
+        if (name.equals("whiteship")) {
+            return new WhiteShip();
+        } else if (name.equals("blackship")) {
+            return new BlackShip();
+        }
+
+        throw new IllegalArgumentException();
+    }
+}
