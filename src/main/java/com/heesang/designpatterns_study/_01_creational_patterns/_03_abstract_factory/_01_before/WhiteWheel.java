3eca2ad1fa8211d859cfe42cc0c6e332d8248a7f
heesang <dlagmltkd12@naver.com>
2022-04-29 22:04:23 +0900

    refactor : 추상 팩토리 패턴 적용 후 WhiteWheel Wheel 인터페이스 implements

--- src/main/java/com/heesang/designpatterns_study/_01_creational_patterns/_03_abstract_factory/_01_before/WhiteWheel.java
+++ src/main/java/com/heesang/designpatterns_study/_01_creational_patterns/_03_abstract_factory/_01_before/WhiteWheel.java
@@ -1,4 +1,6 @@
 package com.heesang.designpatterns_study._01_creational_patterns._03_abstract_factory._01_before;
 
-public class WhiteWheel {
+import com.heesang.designpatterns_study._01_creational_patterns._03_abstract_factory._02_after.Wheel;
+
+public class WhiteWheel implements Wheel {
 }
