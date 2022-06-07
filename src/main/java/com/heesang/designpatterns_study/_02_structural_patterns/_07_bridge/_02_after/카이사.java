c5b8e9386912f2783a5824437168fbc173dda5a3
heesang <dlagmltkd12@naver.com>
2022-06-08 01:54:04 +0900

    feat : 브릿지 타입 패턴 적용 후 카이사 클래스 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_07_bridge/_02_after/카이사.java
@@ -0,0 +1,8 @@
+package com.heesang.designpatterns_study._02_structural_patterns._07_bridge._02_after;
+
+public class 카이사 extends DefaultChampion {
+
+    public 카이사(Skin skin) {
+        super("카이사", skin);
+    }
+}
