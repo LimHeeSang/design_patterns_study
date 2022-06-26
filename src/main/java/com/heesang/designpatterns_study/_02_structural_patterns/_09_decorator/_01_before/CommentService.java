55293b422a80968a939f230552fb8d2df631762e
heesang <dlagmltkd12@naver.com>
2022-06-26 17:36:42 +0900

    feat : 데코레이터 패턴 적용 전 CommentService 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_09_decorator/_01_before/CommentService.java
@@ -0,0 +1,8 @@
+package com.heesang.designpatterns_study._02_structural_patterns._09_decorator._01_before;
+
+public class CommentService {
+
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
