331d18ef4124904e70ebdfcfad4bc411252e7341
heesang <dlagmltkd12@naver.com>
2022-06-26 17:55:41 +0900

    feat : 데코레이터 패턴 적용 후 DefaultCommentService 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_09_decorator/_02_after/DefaultCommentService.java
@@ -0,0 +1,9 @@
+package com.heesang.designpatterns_study._02_structural_patterns._09_decorator._02_after;
+
+public class DefaultCommentService implements CommentService {
+
+    @Override
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
