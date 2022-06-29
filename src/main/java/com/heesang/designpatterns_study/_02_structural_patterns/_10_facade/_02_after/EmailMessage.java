9b083951ae719951781ec401fdec2ce3ff903c87
heesang <dlagmltkd12@naver.com>
2022-06-29 21:59:12 +0900

    feat : EmailMessage setter 제거

--- src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_10_facade/_02_after/EmailMessage.java
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_10_facade/_02_after/EmailMessage.java
@@ -23,39 +23,23 @@ public class EmailMessage {
         return from;
     }
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
     public String getTo() {
         return to;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
 
     public String getSubject() {
         return subject;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
 
     public String getCc() {
         return cc;
     }
 
-    public void setCc(String cc) {
-        this.cc = cc;
-    }
 }
