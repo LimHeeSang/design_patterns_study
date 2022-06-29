946ff737fb2002754b6bff5bce664e3d064f3faf
heesang <dlagmltkd12@naver.com>
2022-06-29 21:41:54 +0900

    feat : 퍼사드 패턴 Spring 사용 예시

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_10_facade/_03_java/FacadeInSpring.java
@@ -0,0 +1,10 @@
+package com.heesang.designpatterns_study._02_structural_patterns._10_facade._03_java;
+
+public class FacadeInSpring {
+
+    public static void main(String[] args) {
+        //MailSender mailSender = new JavaMailSenderImpl();
+
+        //PlatformTransactionManager platformTransactionManager = new JdbcTransactionManager();
+    }
+}
