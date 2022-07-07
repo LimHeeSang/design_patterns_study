e95ec5a5484571ca8b93706901633de1e596d933
heesang <dlagmltkd12@naver.com>
2022-07-08 01:19:22 +0900

    feat : 플라이웨이터 패턴 적용 후 Font 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_11_flyweight/_02_after/Font.java
@@ -0,0 +1,13 @@
+package com.heesang.designpatterns_study._02_structural_patterns._11_flyweight._02_after;
+
+public final class Font {
+
+    private final String font;
+
+    private final int size;
+
+    public Font(String font, int size) {
+        this.font = font;
+        this.size = size;
+    }
+}
