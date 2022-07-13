801409382ec86042cfdb77b6b440fc2b84c13f49
heesang <dlagmltkd12@naver.com>
2022-07-13 16:37:31 +0900

    feat : 프록시 패턴 인터페이스 적용 후 GameService 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_12_proxy/_02_after/_02_interface/GameService.java
@@ -0,0 +1,6 @@
+package com.heesang.designpatterns_study._02_structural_patterns._12_proxy._02_after._02_interface;
+
+public interface GameService {
+
+    void startGame() throws InterruptedException;
+}
