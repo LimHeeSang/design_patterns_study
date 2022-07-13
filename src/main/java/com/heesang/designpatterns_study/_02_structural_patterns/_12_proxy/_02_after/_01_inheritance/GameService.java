d3b1629801923df505847e2182000840e1a50fe8
heesang <dlagmltkd12@naver.com>
2022-07-13 16:28:16 +0900

    feat : 프록시 패턴 상속으로 적용 후 GameService 구현

---
+++ src/main/java/com/heesang/designpatterns_study/_02_structural_patterns/_12_proxy/_02_after/_01_inheritance/GameService.java
@@ -0,0 +1,11 @@
+package com.heesang.designpatterns_study._02_structural_patterns._12_proxy._02_after._01_inheritance;
+
+public class GameService {
+
+    public void startGame() throws InterruptedException {
+        System.out.println("이 자리에 오신 여러분을 진심으로 환영합니다.");
+        //게임에 걸리는 시간입니다.
+        Thread.sleep(1000L);
+    }
+
+}
